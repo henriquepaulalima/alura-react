f34ef3bc8695f66ca8ce936dedc4e1e3ccc62bcc
user-nexus <henrique.p.lima0@gmail.com>
2022-03-16 10:35:53 -0300

    Sending project ceep files

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,7 +1,17 @@
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
