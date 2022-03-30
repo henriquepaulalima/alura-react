94d2b77b6932ce4be2ee53811dd2b758f6cdaaa7
user-nexus <henrique.p.lima0@gmail.com>
2022-03-30 15:01:26 -0300

    Update project files

---
+++ ceep-v2.0/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[],
+      categorias:[],
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  deletarNota(index) {
+    let arrayDeNotas = this.state.notas;
+    arrayDeNotas.splice(index, 1);
+    this.setState({notas:arrayDeNotas})
+  }
+
+  adicionarCategoria(nomeCategoria) {
+    const novoArrayDeCategorias = [...this.state.categorias, nomeCategoria];
+    const novoEstado = {...this.state, categorias:novoArrayDeCategorias};
+    this.setState(novoEstado);
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <main className="conteudo-principal">
+          <ListaDeCategorias 
+            categorias={this.state.categorias}
+            adicionarCategoria={this.adicionarCategoria.bind(this)}/>
+          <ListaDeNotas 
+          apagarNota={this.deletarNota.bind(this)}
+          notas={this.state.notas}/>
+        </main>
+      </section>
+    );
+  }
+}
+
+//new ListaDeNotas({notas:this.notas})
+export default App;
