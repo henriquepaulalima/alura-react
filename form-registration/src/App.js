27c48944f837fed80a5b2afe4073c96e857ff38e
user-nexus <henrique.p.lima0@gmail.com>
2022-05-02 09:23:10 -0300

    Add form-registration project

---
+++ form-registration/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+import FormularioCadastro from './component/FormularioCadastro/FormularioCadastro';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Formulário de Cadastro</h1>
+        <FormularioCadastro />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
