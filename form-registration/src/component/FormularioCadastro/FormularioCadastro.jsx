9b457a941879ff3d010c977ea98d751370c2f12e
user-nexus <henrique.p.lima0@gmail.com>
2022-05-04 09:14:45 -0300

    Update FormularioCadastro component with hooks

--- form-registration/src/component/FormularioCadastro/FormularioCadastro.jsx
+++ form-registration/src/component/FormularioCadastro/FormularioCadastro.jsx
@@ -1,40 +1,59 @@
-import React from 'react';
+import { React, useState} from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@mui/material';
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+
   return (
-    <form>
-      <TextField 
-        id="nome" 
-        label="Nome" 
-        variant="outlined" 
-        fullWidth 
-        margin="normal"
-      />
-      
-      <TextField 
-        id="sobrenome" 
-        label="Sobrenome" 
-        variant="outlined" 
+    <form 
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome, sobrenome);
+      }}
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => {
+          let tmpNome = event.target.value;
+          if(tmpNome.length >= 20) {
+            tmpNome = tmpNome.substr(0, 20);
+          }
+          setNome(tmpNome);
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
         fullWidth
         margin="normal"
       />
 
-      <TextField 
-        id="cpf" 
-        label="CPF" 
-        variant="outlined" 
+      <TextField
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
         fullWidth
         margin="normal"
       />
 
       <FormControlLabel control={<Switch defaultChecked />} label="Promoções" />
       <FormControlLabel control={<Switch defaultChecked />} label="Novidades" />
-      
+
       <Button type="submit" variant="contained">Cadastrar</Button>
     </form>
-    );
-  }
-  
-  export default FormularioCadastro;
-  
+  );
+}
+
+export default FormularioCadastro;
