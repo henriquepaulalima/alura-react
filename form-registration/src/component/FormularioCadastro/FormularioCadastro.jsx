1543861c16dc0d6256d1c642c5d5ac586f8cdff6
user-nexus <henrique.p.lima0@gmail.com>
2022-05-11 07:33:50 -0300

    Update full course files

--- form-registration/src/component/FormularioCadastro/FormularioCadastro.jsx
+++ form-registration/src/component/FormularioCadastro/FormularioCadastro.jsx
@@ -1,12 +1,13 @@
 import { React, useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@mui/material';
 
-function FormularioCadastro({aoEnviar}) {
+function FormularioCadastro({aoEnviar, validarCpf}) {
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(true);
+  const [erros, setErros] = useState({cpf:{valido:true, texto:""}});
 
   return (
     <form
@@ -44,6 +45,12 @@ function FormularioCadastro({aoEnviar}) {
         onChange={(event) => {
           setCpf(event.target.value);
         }}
+        onBlur={(event) => {
+          const ehValido = validarCpf(cpf);
+          setErros({cpf:ehValido})
+        }}
+        error={!erros.cpf.valido}
+        helperText={erros.cpf.texto}
         id="cpf"
         label="CPF"
         variant="outlined"
