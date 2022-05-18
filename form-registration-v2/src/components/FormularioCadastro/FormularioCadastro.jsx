30395fd150826bf74d9a08a46aa89472fcf94d41
user-nexus <henrique.p.lima0@gmail.com>
2022-05-18 08:47:50 -0300

    Add form switch

--- form-registration-v2/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ form-registration-v2/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -4,13 +4,18 @@ import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
-  return (
-    <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+  const [etapaAtual, setEtapaAtual] = useState(0);
+  const formularios = [
+    <DadosUsuario aoEnviar={proximo}/>, 
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>, 
+    <DadosEntrega aoEnviar={aoEnviar}/>
+  ];
+
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
+  }
+
+  return <>{formularios[etapaAtual]}</>;
 }
 
 export default FormularioCadastro;
