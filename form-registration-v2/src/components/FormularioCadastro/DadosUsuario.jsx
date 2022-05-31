89afa50ea5c6f026a64ec37765cad2e202657367
user-nexus <henrique.p.lima0@gmail.com>
2022-05-30 21:24:38 -0300

    Add hooks to forms and add stepper

--- form-registration-v2/src/components/FormularioCadastro/DadosUsuario.jsx
+++ form-registration-v2/src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,24 +1,36 @@
 import { Button, TextField } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 
-function DadosUsuario({aoEnviar}) {
+function DadosUsuario({ aoEnviar }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
-    <form onSubmit={(event) => {
-      event.preventDefault();
-      aoEnviar();
-    }}>
-      <TextField 
-        id='email' 
-        label='Email' 
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ email, senha });
+      }}>
+      <TextField
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+        id='email'
+        label='Email'
         type='email'
         required
         variant="outlined"
         margin="normal"
         fullWidth
       />
-      <TextField 
-        id='senha' 
-        label='Senha' 
+      <TextField
+        value={senha}
+        onChange={(event) => {
+          setSenha(event.target.value);
+        }}
+        id='senha'
+        label='Senha'
         type='password'
         required
         variant="outlined"
